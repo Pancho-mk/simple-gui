7a8561a7edafcbc9526458f13539fbb5680acd54
Pancho-mk <dpance@yahoo.com>
2020-04-13 16:11:10 +0200

    added simple_gui.py

---
+++ simple_gui.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+apps = [] 
+
+if os.path.isfile('save.txt'):
+	with open('save.txt', 'r') as f:
+		tempApps = f.read()
+		tempApps = tempApps.split(',')
+		apps = [x for x in tempApps if x.strip()]
+
+def addApp():
+	for widget in frame.winfo_children():
+		widget.destroy()
+
+	filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+    filetypes=(("executables", "*.*"), ("all files", "*.*")))
+	apps.append(filename)
+	print(filename)
+	for app in apps:
+		label = tk.Label(frame, text=app, bg="gray")
+		label.pack()
+
+def runApps():
+	for app in apps:
+		os.system(app)                         # os.startfile(app) in Windows
+
+
+canvas = tk.Canvas(root, height=600, width=500, bg="#34495e")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, 
+	                 pady=5, fg="white", bg="#34495e", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, 
+	                 pady=5, fg="white", bg="#34495e", command=runApps)
+
+runApps.pack()
+
+
+for app in apps:                       # start app from file
+	label = tk.Label(frame, text=app)
+	label.pack()
+
+
+root.mainloop()
+
+with open('save.text', 'w') as f:
+	for app in apps:
+		f.write(app + ',')
